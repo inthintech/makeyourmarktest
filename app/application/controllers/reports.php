8d17e846daccef0142abb62f049fe5a37de3795e
sytscompany <contactsyts@gmail.com>
2015-06-07 23:55:55 +0530

    ultra

--- app/application/controllers/reports.php
+++ app/application/controllers/reports.php
@@ -102,16 +102,17 @@ class Reports extends CI_Controller {
 		switch($reportId)
 		{
 			case 1:
-			$this->passPercentageReport($this->input->post('examid'),$this->input->post('levelid'));
+			$this->passPercentageReport($this->input->post('examid'),$this->input->post('levelid'),$client_name);
 			break;
 			case 2:
+			$this->topperReport($this->input->post('examid'),$this->input->post('levelid'),$client_name);
 			break;
 		}
 
 		$this->load->view('footer');
 	}
 
-	private function passPercentageReport($examid,$levelid)
+	private function passPercentageReport($examid,$levelid,$client_name)
 
 	{
 
@@ -158,7 +159,7 @@ class Reports extends CI_Controller {
 			foreach($output as $row)
 				{
 				
-					$opt_data = $opt_data."<tr><td>".$row->client_name."</td><td>".$row->student_cnt."</td>
+					$opt_data = $opt_data."<tr><td>".$client_name."</td><td>".$row->student_cnt."</td>
 					<td>".$row->student_pass_cnt."</td><td>".$row->pass_percentage."</td></tr>";
 				
 				} 
@@ -403,6 +404,191 @@ class Reports extends CI_Controller {
 		
 	}
 
+/*--------------------------------- TOPPER REPORT ---------------------------------*/
+
+	private function topperReport($examid,$levelid,$client_name)
+
+	{
+
+		$result = $this->user->getExamName($examid);
+		foreach($result as $row)
+		{
+			$exam_name= $row->exam_name;
+		} 
+		
+		$filterQry = "where 1=1 and rank=1";
+
+		/* College Level Report*/
+		
+		if($levelid==1)
+		{
+			
+			/*
+			if($this->input->post('deptfilter')<>99)
+			{
+				$filterQry = $filterQry." and dept_code='".$this->input->post('deptfilter')."'";
+			}
+			if($this->input->post('yearfilter')<>99)
+			{
+				$filterQry = $filterQry." and year=".$this->input->post('yearfilter');
+			}
+			if($this->input->post('sectionfilter')<>99)
+			{
+				$filterQry = $filterQry." and section='".$this->input->post('sectionfilter')."'";
+			}
+			if($this->input->post('subjectfilter')<>99)
+			{
+				$filterQry = $filterQry." and subject_code='".$this->input->post('subjectfilter')."'";
+			}
+			*/
+			$output = $this->analysis->topperReportCollege($this->session->userdata('client_id'),$examid,$filterQry);
+			
+			if($output)
+			{
+			$table_headers = "<th style=\"width:40%;\">College Name</th>
+			<th>Student ID</th><th>Student Name</th><th>Class</th>
+			<th>Percentage</th>";
+
+			$opt_data = '';
+			foreach($output as $row)
+				{
+				
+					$opt_data = $opt_data."<tr><td>".$client_name."</td><td>".$row->student_id."</td>
+					<td>".$row->student_name."</td>
+					<td>".$row->dept_code." ".$row->year." ".$row->section."</td>
+					<td>".$row->percentage."</td>
+					</tr>";
+				
+				} 
+			
+			$rptdata = array('exam_name' => $exam_name,'table_headers' => $table_headers,'data' => $opt_data,'report_name' => 'Topper Report','level' => 'College Level');
+			$this->load->view('vrptoutput',$rptdata);
+			}
+			else
+			{
+				$statusdata = array('message' => '<div class="alert alert-danger" role="alert">No data was returned. Either there is no data for the input or there are any filters applied. Please remove any unneccessary filter and try again.</div>');
+				$this->load->view('vmessage',$statusdata);
+			}
+
+		}
+		
+
+/* Dept Level Report*/
+		
+		if($levelid==2)
+		{
+			
+			if($this->input->post('deptfilter')<>99)
+			{
+				$filterQry = $filterQry." and dept_code='".$this->input->post('deptfilter')."'";
+			}
+
+			/*
+			
+			if($this->input->post('yearfilter')<>99)
+			{
+				$filterQry = $filterQry." and year=".$this->input->post('yearfilter');
+			}
+			if($this->input->post('sectionfilter')<>99)
+			{
+				$filterQry = $filterQry." and section='".$this->input->post('sectionfilter')."'";
+			}
+			if($this->input->post('subjectfilter')<>99)
+			{
+				$filterQry = $filterQry." and subject_code='".$this->input->post('subjectfilter')."'";
+			}
+			*/
+			$output = $this->analysis->topperReportDept($this->session->userdata('client_id'),$examid,$filterQry);
+			
+			if($output)
+			{
+			$table_headers = "<th style=\"width:10%;\">Dept</th>
+			<th>Student ID</th><th>Student Name</th><th>Class</th>
+			<th>Percentage</th>";
+
+			$opt_data = '';
+			foreach($output as $row)
+				{
+				
+					$opt_data = $opt_data."<tr><td>".$row->dept_code."</td><td>".$row->student_id."</td>
+					<td>".$row->student_name."</td>
+					<td>".$row->dept_code." ".$row->year." ".$row->section."</td>
+					<td>".$row->percentage."</td>
+					</tr>";
+				
+				} 
+			
+			$rptdata = array('exam_name' => $exam_name,'table_headers' => $table_headers,'data' => $opt_data,'report_name' => 'Topper Report','level' => 'Department Level');
+			$this->load->view('vrptoutput',$rptdata);
+			}
+			else
+			{
+				$statusdata = array('message' => '<div class="alert alert-danger" role="alert">No data was returned. Either there is no data for the input or there are any filters applied. Please remove any unneccessary filter and try again.</div>');
+				$this->load->view('vmessage',$statusdata);
+			}
+
+		}
+
+/* Year Level Report*/
+		
+		if($levelid==3)
+		{
+			
+			if($this->input->post('yearfilter')<>99)
+			{
+				$filterQry = $filterQry." and year=".$this->input->post('yearfilter');
+			}
+
+			/*
+
+			if($this->input->post('deptfilter')<>99)
+			{
+				$filterQry = $filterQry." and dept_code='".$this->input->post('deptfilter')."'";
+			}
+			
+			
+			if($this->input->post('sectionfilter')<>99)
+			{
+				$filterQry = $filterQry." and section='".$this->input->post('sectionfilter')."'";
+			}
+			if($this->input->post('subjectfilter')<>99)
+			{
+				$filterQry = $filterQry." and subject_code='".$this->input->post('subjectfilter')."'";
+			}
+			*/
+			$output = $this->analysis->topperReportYear($this->session->userdata('client_id'),$examid,$filterQry);
+			
+			if($output)
+			{
+			$table_headers = "<th style=\"width:10%;\">Year</th>
+			<th>Student ID</th><th>Student Name</th><th>Class</th>
+			<th>Percentage</th>";
+
+			$opt_data = '';
+			foreach($output as $row)
+				{
+				
+					$opt_data = $opt_data."<tr><td>".$row->year."</td><td>".$row->student_id."</td>
+					<td>".$row->student_name."</td>
+					<td>".$row->dept_code." ".$row->year." ".$row->section."</td>
+					<td>".$row->percentage."</td>
+					</tr>";
+				
+				} 
+			
+			$rptdata = array('exam_name' => $exam_name,'table_headers' => $table_headers,'data' => $opt_data,'report_name' => 'Topper Report','level' => 'Year Level');
+			$this->load->view('vrptoutput',$rptdata);
+			}
+			else
+			{
+				$statusdata = array('message' => '<div class="alert alert-danger" role="alert">No data was returned. Either there is no data for the input or there are any filters applied. Please remove any unneccessary filter and try again.</div>');
+				$this->load->view('vmessage',$statusdata);
+			}
+
+		}
+
+	
+	}
 
 	
 }
--- app/application/models/analysis.php
+++ app/application/models/analysis.php
@@ -8,18 +8,16 @@ function passPercentageReportCollege($client_id,$exam_id,$filterQry)
 {
 
 	$query = $this->db->query("select * from
-	(select c.client_name,a.student_cnt,b.student_pass_cnt,
-    ROUND((b.student_pass_cnt/a.student_cnt)*100) pass_percentage from clients c
-    join
+	(select a.student_cnt,b.student_pass_cnt,
+    ROUND((b.student_pass_cnt/a.student_cnt)*100) pass_percentage from 
     (select client_id,count(distinct student_id) student_cnt from results
     where lgcl_del_f='N' and client_id=".$this->db->escape($client_id)." and exam_id=".$this->db->escape($exam_id)." group by client_id)a
-    on c.client_id=a.client_id
+    
     join
     (select client_id,count(distinct student_id) student_pass_cnt from results
-    where marks_obtained>pass_mark and lgcl_del_f='N' 
+    where marks_obtained>=pass_mark and lgcl_del_f='N' 
     and client_id=".$this->db->escape($client_id)." and exam_id=".$this->db->escape($exam_id)." group by client_id)b
-    on c.client_id=b.client_id
-    where c.client_id=".$this->db->escape($client_id).")
+    on a.client_id=b.client_id)
 	SCR ".$filterQry);
 
    if($query -> num_rows() >= 1)
@@ -45,7 +43,7 @@ function passPercentageReportDept($client_id,$exam_id,$filterQry)
     where lgcl_del_f='N' and client_id=".$this->db->escape($client_id)." and exam_id=".$this->db->escape($exam_id)." group by dept_code)a
     join
     (select dept_code,count(distinct student_id) student_pass_cnt from results
-    where marks_obtained>pass_mark and lgcl_del_f='N' 
+    where marks_obtained>=pass_mark and lgcl_del_f='N' 
     and client_id=".$this->db->escape($client_id)." and exam_id=".$this->db->escape($exam_id)." group by dept_code)b
     on a.dept_code=b.dept_code)
 	SCR ".$filterQry." order by pass_percentage desc");
@@ -72,7 +70,7 @@ function passPercentageReportYear($client_id,$exam_id,$filterQry)
     where lgcl_del_f='N' and client_id=".$this->db->escape($client_id)." and exam_id=".$this->db->escape($exam_id)." group by year)a
     join
     (select year,count(distinct student_id) student_pass_cnt from results
-    where marks_obtained>pass_mark and lgcl_del_f='N' 
+    where marks_obtained>=pass_mark and lgcl_del_f='N' 
     and client_id=".$this->db->escape($client_id)." and exam_id=".$this->db->escape($exam_id)." group by year)b
     on a.year=b.year)
   SCR ".$filterQry." order by pass_percentage desc");
@@ -99,7 +97,7 @@ function passPercentageReportDeptYear($client_id,$exam_id,$filterQry)
     where lgcl_del_f='N' and client_id=".$this->db->escape($client_id)." and exam_id=".$this->db->escape($exam_id)." group by dept_code,year)a
     join
     (select dept_code,year,count(distinct student_id) student_pass_cnt from results
-    where marks_obtained>pass_mark and lgcl_del_f='N' 
+    where marks_obtained>=pass_mark and lgcl_del_f='N' 
     and client_id=".$this->db->escape($client_id)." and exam_id=".$this->db->escape($exam_id)." group by dept_code,year)b
     on a.dept_code=b.dept_code and a.year=b.year)
   SCR ".$filterQry." order by pass_percentage desc");
@@ -126,7 +124,7 @@ function passPercentageReportClass($client_id,$exam_id,$filterQry)
     where lgcl_del_f='N' and client_id=".$this->db->escape($client_id)." and exam_id=".$this->db->escape($exam_id)." group by dept_code,year,section)a
     join
     (select dept_code,year,section,count(distinct student_id) student_pass_cnt from results
-    where marks_obtained>pass_mark and lgcl_del_f='N' 
+    where marks_obtained>=pass_mark and lgcl_del_f='N' 
     and client_id=".$this->db->escape($client_id)." and exam_id=".$this->db->escape($exam_id)." group by dept_code,year,section)b
     on a.dept_code=b.dept_code and a.year=b.year)
   SCR ".$filterQry." order by pass_percentage desc");
@@ -141,6 +139,131 @@ function passPercentageReportClass($client_id,$exam_id,$filterQry)
    }
 
 }
+
+function topperReportCollege($client_id,$exam_id,$filterQry)
+
+{
+
+  $query = $this->db->query("select dept_code,year,section,student_id,student_name,percentage,rank from
+(select rset.*,
+( 
+CASE 
+WHEN percentage=@oldPercent
+THEN @curRow := @curRow + 0 
+ELSE @curRow := @curRow + 1 
+END
+)AS rank,
+@oldPercent := percentage
+from
+(select b.*,a.percentage
+from
+(select student_id,AVG(marks_obtained) percentage
+from results
+where lgcl_del_f='N' and exam_id=".$this->db->escape($exam_id)." and client_id=".$this->db->escape($client_id)." and marks_obtained>=pass_mark
+group by student_id)a
+join
+(select distinct client_id,dept_code,year,section,student_id,student_name
+from results
+where lgcl_del_f='N' and exam_id=".$this->db->escape($exam_id)." and client_id=".$this->db->escape($client_id)." and marks_obtained>=pass_mark)b
+on a.student_id=b.student_id)
+rset,
+(SELECT @curRow := 0, @oldPercent := 0) r
+order by percentage desc
+)SCR ".$filterQry);
+
+   if($query -> num_rows() >= 1)
+   {
+     return $query->result();
+   }
+   else
+   {
+     return false;
+   }
+
+}
+
+function topperReportDept($client_id,$exam_id,$filterQry)
+
+{
+
+  $query = $this->db->query("select dept_code,year,section,student_id,student_name,percentage,rank from
+(select rset.*,
+( 
+CASE 
+WHEN dept_code = @dept and percentage=@oldPercent
+THEN @curRow := @curRow + 0 
+WHEN dept_code = @dept and percentage<>@oldPercent
+THEN @curRow := @curRow + 1 
+ELSE @curRow := 1 AND @dept := dept_code END
+) + 1 AS rank,@oldPercent := percentage
+from
+(select b.*,a.percentage
+from
+(select student_id,AVG(marks_obtained) percentage
+from results
+where lgcl_del_f='N' and exam_id=".$this->db->escape($exam_id)." and client_id=".$this->db->escape($client_id)." and marks_obtained>=pass_mark
+group by student_id)a
+join
+(select distinct client_id,dept_code,year,section,student_id,student_name
+from results
+where lgcl_del_f='N' and exam_id=".$this->db->escape($exam_id)." and client_id=".$this->db->escape($client_id)." and marks_obtained>=pass_mark)b
+on a.student_id=b.student_id)rset,
+(SELECT @curRow := 0, @oldPercent := 0, @client_id := 0, @dept := '', @year := 0, @section='') r
+order by dept_code,percentage desc
+)SCR ".$filterQry." order by dept_code");
+
+   if($query -> num_rows() >= 1)
+   {
+     return $query->result();
+   }
+   else
+   {
+     return false;
+   }
+
+}
+
+function topperReportYear($client_id,$exam_id,$filterQry)
+
+{
+
+  $query = $this->db->query("select dept_code,year,section,student_id,student_name,percentage,rank from
+(select rset.*,
+( 
+CASE 
+WHEN year = @year and percentage=@oldPercent
+THEN @curRow := @curRow + 0 
+WHEN year = @year and percentage<>@oldPercent
+THEN @curRow := @curRow + 1 
+ELSE @curRow := 1 AND @year := year END
+) AS rank,@oldPercent := percentage
+from
+(select b.*,a.percentage
+from
+(select student_id,AVG(marks_obtained) percentage
+from results
+where lgcl_del_f='N' and exam_id=".$this->db->escape($exam_id)." and client_id=".$this->db->escape($client_id)." and marks_obtained>=pass_mark
+group by student_id)a
+join
+(select distinct client_id,dept_code,year,section,student_id,student_name
+from results
+where lgcl_del_f='N' and exam_id=".$this->db->escape($exam_id)." and client_id=".$this->db->escape($client_id)." and marks_obtained>=pass_mark)b
+on a.student_id=b.student_id)rset,
+(SELECT @curRow := 0, @oldPercent := 0, @client_id := 0, @dept := '', @year := 0, @section :='') r
+order by year,percentage desc
+)SCR ".$filterQry." order by year");
+
+   if($query -> num_rows() >= 1)
+   {
+     return $query->result();
+   }
+   else
+   {
+     return false;
+   }
+
+}
+
 
 }
 ?>
